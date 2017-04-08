88619a7d20e2068aa149a10ecf14943f9f57558a
moxdev <moxdev1@gmail.com>
2017-04-08 18:50:35 -0400

    footer

--- footer.php
+++ footer.php
@@ -27,15 +27,28 @@
                     ?>
 
                     <?php echo file_get_contents("http://localhost:8888/leading-minds/wp-content/themes/leading_minds/imgs/footer-logo.svg"); ?>
-                    <p><?php echo esc_html( $phone ); ?></p>
-                    <p><?php echo esc_html( $email ); ?></p>
+
+                    <a class="contact-link" href="<?php echo esc_html( $phone ); ?>"><?php echo esc_html( $phone ); ?></a>
+                    <a class="contact-link" href="<?php echo esc_html( $email ); ?>"><?php echo esc_html( $email ); ?></a>
 
                     <?php
                 } ?>
 
             </div><!-- contact-wrapper -->
-            <div class="social-wrapper"></div>
-            <div class="privacy-wrapper"></div>
+            <div class="social-wrapper">
+
+                <?php echo file_get_contents('http://localhost:8888/leading-minds/wp-content/themes/leading_minds/imgs/twitter-logo.svg'); ?>
+                <?php echo file_get_contents('http://localhost:8888/leading-minds/wp-content/themes/leading_minds/imgs/linkedin-logo.svg'); ?>
+                <?php echo file_get_contents('http://localhost:8888/leading-minds/wp-content/themes/leading_minds/imgs/bbb-logo.svg'); ?>
+                <?php echo file_get_contents('http://localhost:8888/leading-minds/wp-content/themes/leading_minds/imgs/pcc-logo.svg'); ?>
+            </div>
+            <div class="privacy-wrapper">
+
+                <a href="<?php echo esc_url( get_page_link( get_page_by_title( 'Terms and Conditions' ) ) ); ?>">Terms and Conditions</a>
+
+                <a href="<?php echo esc_url( get_page_link( get_page_by_title( 'Privacy Policy' ) ) ); ?>">Privacy Policy</a>
+
+            </div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
