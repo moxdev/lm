f6cf17533a087bfd728a7286259618a4105e68d6
moxdev <moxdev1@gmail.com>
2017-04-09 20:54:40 -0400

    rewrite unsuccessful

--- inc/flexible-content-module.php
+++ inc/flexible-content-module.php
@@ -162,20 +162,30 @@ function leading_minds_flexible_content_module() {
 
             elseif( get_row_layout() == 'advanced_content_section' ):
 
-                $bg_color            = get_sub_field('section_background_color');
-                $img                 = get_sub_field('image');
-                $header              = get_sub_field('header');
-                $sub_header          = get_sub_field('sub_header');
-                $editor              = get_sub_field('editor');
-                $add_split_column    = get_sub_field('add_a_split_column_text_section');
-                $left_column_text    = get_sub_field('left_column_text');
-                $right_column_text   = get_sub_field('right_column_text');
-                $add_content_footer  = get_sub_field('add_a_content_footer_section');
-                $content_footer      = get_sub_field('content_footer');
+                $bg_color             = get_sub_field('section_background_color');
+                $bg_img               = get_sub_field('section_background_color');
 
-                ?>
-                <section class="advanced-content-section wrapper" style="background-color:<?php echo $bg_color; ?>">
-                    <div class="content-section-wrapper">
+                $img                  = get_sub_field('image');
+                $header               = get_sub_field('header');
+                $sub_header           = get_sub_field('sub_header');
+                $editor               = get_sub_field('editor');
+
+                $add_split_column     = get_sub_field('add_a_split_column_text_section');
+                $add_content_footer   = get_sub_field('add_a_content_footer_section');
+                $add_skills           = get_sub_field('add_skills_section');
+                $add_secondary_editor = get_sub_field( 'add_seconday_editor' );
+
+                if ( $bg_img ) { ?>
+
+                <section class="advanced-content-section" style="background-color:<?php echo $bg_color; ?>;background-image:url(http://localhost:8888/test-site/wp-content/themes/test/imgs/brains.svg);">
+
+                <?php }else { ?>
+
+                <section class="advanced-content-section" style="background-color:<?php echo $bg_color; ?>">
+
+                <?php } ?>
+
+                    <div class="content-section-wrapper wrapper">
 
                         <?php
 
@@ -183,19 +193,26 @@ function leading_minds_flexible_content_module() {
                                 <img class="header-img" src="<?php echo $img['sizes']['thumbnail']; ?>" alt="<?php echo $img['alt']; ?>" description="<?php echo $img['description']; ?>">
                             <?php }
 
+                            // Header
                             if ($header) { ?>
                                 <h2><?php echo esc_html( $header ); ?></h2>
                             <?php }
 
+                            //Sub Header
                             if ($sub_header) { ?>
                                 <h3><?php echo esc_html( $sub_header ); ?></h3>
                             <?php }
 
+                            // Editor
                             if ($editor) { ?>
                                 <?php echo $editor; ?>
                             <?php }
 
-                            if ($add_split_column) { ?>
+                            // Split Column
+                            if ($add_split_column) {
+
+                                $left_column_text  = get_sub_field('left_column_text');
+                                $right_column_text = get_sub_field('right_column_text'); ?>
 
                                 <div class="split-column-wrapper">
                                     <div class="left-column">
@@ -216,74 +233,56 @@ function leading_minds_flexible_content_module() {
 
                             <?php }
 
-                            if( have_rows('content_footer') ):
+                            // Skills
+                            if ($add_skills) {
 
-                                ?>
+                                $skills = get_sub_field('skills');
 
-                                <div class="content-footer-wrapper">
+                                if( have_rows('skills') ): ?>
 
-                                <?php
+                                    <div class="skills-wrapper">
 
-                                while ( have_rows('content_footer') ) : the_row();
+                                    <?php while( have_rows('skills') ): the_row();
 
-                                    if( get_row_layout() == 'standard_button_link' ):
-
-                                        $url = get_sub_field('button_url');
-                                        $text = get_sub_field('button_text');
+                                        $skill = get_sub_field('skill');
 
                                         ?>
 
-                                        <a href="<?php echo esc_url( $url ); ?>"><button><?php echo esc_html( $text ); ?></button></a>
+                                        <div class="skill">
 
-                                        <?php
+                                            <?php if( !empty($skill) ) : ?>
+                                                <?php echo esc_html( $skill); ?>
+                                            <?php endif; ?>
 
-                                    elseif( get_row_layout() == 'call_phone_button' ):
+                                        </div>
 
-                                        $phone = get_sub_field('phone_number');
-                                        $text = get_sub_field('button_text');
+                                    <?php endwhile; ?>
 
-                                        ?>
+                                    </div><!-- skills-wrapper -->
 
-                                        <a href="tel:<?php echo esc_html( $phone ); ?>"><button><?php echo esc_html( $text ); ?></button></a>
+                                <?php endif;
 
-                                        <?php
+                            }
 
-                                    elseif( get_row_layout() == 'email_button' ):
+                            // Secondary Editor
+                            if ($add_secondary_editor) {
 
-                                        $email = get_sub_field('email_address');
-                                        $text = get_sub_field('button_text');
+                                $secondary_editor = get_sub_field('secondary_editor');
 
-                                        ?>
+                                if ($secondary_editor) {
 
-                                        <a href="mailto:<?php echo esc_html( $email ); ?>"><button><?php echo esc_html( $text ); ?></button></a>
+                                    echo $secondary_editor;
 
-                                        <?php
+                                }
+                            }
 
-                                    elseif( get_row_layout() == 'image' ):
+                            // Content Footer
 
-                                        $img = get_sub_field('image');
-
-                                        ?>
-
-                                        <img class="footer-img" src="<?php echo $img['sizes']['flexible-content-module-footer-image']; ?>" alt="<?php echo $img['alt']; ?>" description="<?php echo $img['description']; ?>">
-
-                                        <?php
-
-                                    endif;
-
-                                endwhile;
-
-                                ?>
-
-                                </div><!-- content-footer-wrapper -->
-
-                                <?php
-
-                            endif;
 
                         ?>
 
-                    </div>
+                    </div><!-- content-section-wrapper -->
+
                 </section>
                 <?php
 
