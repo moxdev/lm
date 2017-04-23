18738e9cccedf216a1ddfa0a0f092eb4a0dd8322
moxdev <moxdev1@gmail.com>
2017-04-22 20:04:04 -0400

    added center brain gray section

--- inc/acf-color-content-section.php
+++ inc/acf-color-content-section.php
@@ -18,6 +18,16 @@ function leading_minds_color_content_section() {
             $title = get_sub_field('title');
             $editor = get_sub_field('editor');
 
+            $left_title = get_sub_field('left_section_title');
+            $left_editor = get_sub_field('left_section_editor');
+            $left_btn_label = get_sub_field('left_section_button_label');
+            $left_link = get_sub_field('left_section_button_link');
+
+            $right_title = get_sub_field('right_section_title');
+            $right_editor = get_sub_field('right_section_editor');
+            $right_btn_label = get_sub_field('right_section_button_label');
+            $right_link = get_sub_field('right_section_button_link');
+
             ?>
 
             <section class="color-callout-section">
@@ -49,6 +59,54 @@ function leading_minds_color_content_section() {
                                     <?php echo $editor; ?>
 
                                 <?php endif; ?>
+
+                            </div>
+
+                            <div class="split-section-wrapper">
+
+                                <div class="left-section-wrapper">
+
+                                    <?php if( $left_title ) : ?>
+
+                                        <h3><?php echo esc_html($left_title); ?></h3>
+
+                                    <?php endif; ?>
+
+                                    <?php if( $left_editor ) : ?>
+
+                                        <?php echo $left_editor; ?>
+
+                                    <?php endif; ?>
+
+                                    <?php if( $left_btn_label ) : ?>
+
+                                        <a href="<?php echo esc_url( $left_link ); ?>"><button><?php echo esc_html( $left_btn_label ); ?></button></a>
+
+                                    <?php endif; ?>
+
+                                </div><!-- left-section-wrapper -->
+
+                                <div class="right-section-wrapper">
+
+                                    <?php if( $right_title ) : ?>
+
+                                        <h3><?php echo esc_html($right_title); ?></h3>
+
+                                    <?php endif; ?>
+
+                                    <?php if( $right_editor ) : ?>
+
+                                        <?php echo $right_editor; ?>
+
+                                    <?php endif; ?>
+
+                                    <?php if( $right_btn_label ) : ?>
+
+                                        <a href="<?php echo esc_url( $right_link ); ?>"><button><?php echo esc_html( $right_btn_label ); ?></button></a>
+
+                                    <?php endif; ?>
+
+                                </div><!-- right-section-wrapper -->
 
                             </div>
                         </div>
