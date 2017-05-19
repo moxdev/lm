c7667385610e30670b475d7c05815c6cee50a076
moxdev <moxdev1@gmail.com>
2017-05-18 20:49:19 -0400

    might need to write header

--- header.php
+++ header.php
@@ -24,6 +24,14 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'leading_minds' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<?php $drop_menu = get_field('add_dropdown_menu', 'options');
+
+		    if( $drop_menu ):  ?>
+		        <div class="dropdown-menu">
+		            <?php wp_nav_menu( array( 'theme_location' => 'drop-menu', 'menu_id' => 'drop-menu' ) ); ?>
+		        </div>
+		    <?php endif; ?>
+
 		<div class="header-wrapper">
 			<div class="site-branding">
 
@@ -46,14 +54,6 @@
 				endif; ?>
 			</div><!-- .site-branding -->
 
-			<?php $drop_menu = get_field('add_dropdown_menu', 'options');
-
-			    if( $drop_menu ):  ?>
-			        <div class="dropdown-menu">
-			            <?php wp_nav_menu( array( 'theme_location' => 'drop-menu', 'menu_id' => 'drop-menu' ) ); ?>
-			        </div>
-			    <?php endif; ?>
-
 			<nav id="site-navigation" class="main-navigation" role="navigation">
 
 				<button class="menu-toggle" aria-controls="mobile-menu" aria-expanded="false"><img class="mobile-icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAAsklEQVRoQ+2WMQ7EIAwE4/8/mijl6YopnFXAmrQOK5hBxnUd/tXh+788wNcGNaCBJgGvUBNge/k8A2ut1cYSDKiqH+h/BjxAkP4TPd9AGODr8fO60OuIwoEaCAPGeA0govAP8ww4SqSvjMNcmDDF4zBHAbvV53Wh3QjTfjRAhNJ1DaQJU/48A85C5LxZx5dYA03CtBwNUMBu9XldaDfCtB8NEKF0XQNpwpSvASKUrh9v4AbQsjAxOjNvzAAAAABJRU5ErkJggg==" width="48" height="48"></button>
@@ -65,7 +65,8 @@
 				</div>
 
 			</nav><!-- #site-navigation -->
-		</div>
+		</div><!-- header-wrapper -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
